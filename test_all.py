c5809868689593608d457436d357795b0a5cc84b
MrLokans <trikster1911@gmail.com>
2016-10-17 22:09:06 +0300

    Added extra tests for the movie plot parser.

--- test_all.py
+++ test_all.py
@@ -5,6 +5,8 @@ from utils import (
     extract_id_from_imdbid
 )
 
+from parser import movie_from_text
+
 
 class TestIMDBIDGeneration(object):
     def test_negative_numbers_ignored_by_imdb_generator(self):
@@ -47,3 +49,51 @@ class TestIMDBExtraction(object):
     def test_sample_inputs(self, value, expected):
         result = extract_id_from_imdbid(value)
         assert result == expected
+
+
+@pytest.fixture
+def page_text():
+    with open('test-page.html') as f:
+        return f.read()
+
+
+class TestPageParsing(object):
+    def test_correctly_extracts_movie_title(self, page_text):
+        m = movie_from_text(page_text, 'tt0000001')
+        assert m.title == 'Donnie Darko'
+
+    def test_correctly_extracts_movie_plot(self, page_text):
+        m = movie_from_text(page_text, 'tt0000001')
+        assert m.plot == 'A troubled teenager is plagued by visions of a large bunny rabbit that manipulates him to commit a series of crimes, after narrowly escaping a bizarre accident.'
+
+    def test_correctly_extracts_movie_year(self, page_text):
+        m = movie_from_text(page_text, 'tt0000001')
+        assert m.year == '2001'
+
+    def test_correctly_extracts_movie_genres(self, page_text):
+        m = movie_from_text(page_text, 'tt0000001')
+        assert m.genre == 'Drama, Sci-Fi, Thriller'
+
+    def test_correctly_extracts_movie_metascore(self, page_text):
+        m = movie_from_text(page_text, 'tt0000001')
+        assert m.metascore == '71'
+
+    def test_correctly_extracts_movie_director(self, page_text):
+        m = movie_from_text(page_text, 'tt0000001')
+        assert m.director == 'Richard Kelly'
+
+    def test_correctly_extracts_movie_actors(self, page_text):
+        m = movie_from_text(page_text, 'tt0000001')
+        assert m.actors == 'Jake Gyllenhaal, Holmes Osborne, Maggie Gyllenhaal, Daveigh Chase'
+
+    def test_correctly_extracts_movie_language(self, page_text):
+        m = movie_from_text(page_text, 'tt0000001')
+        assert m.language == 'English'
+
+    def test_correctly_extracts_movie_country(self, page_text):
+        m = movie_from_text(page_text, 'tt0000001')
+        assert m.country == 'USA'
+
+    def test_correctly_extracts_movie_runtime(self, page_text):
+        m = movie_from_text(page_text, 'tt0000001')
+        assert m.runtime == '113 min'
